1142dd2e4dc32bcc913e959e6b7fd1b2b782cab4
Kaio Ferreira <121730814+KaioFerr@users.noreply.github.com>
2025-10-08 18:41:44 -0300

    fix: Fully isolate integration test from external services

--- src/test/java/br/com/orders/order_management/OrderManagementApplicationTests.java
+++ src/test/java/br/com/orders/order_management/OrderManagementApplicationTests.java
@@ -3,7 +3,11 @@ package br.com.orders.order_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+		"spring.data.redis.repositories.enabled=false",
+		"spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
+})
 class OrderManagementApplicationTests {
 
 	@Test
